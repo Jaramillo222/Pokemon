4260de2f8d980d1cd4718c02df11aad4e3ca9df1
Michael Jaramillo <jaramillon222@gmail.com>
2023-12-08 15:02:16 -0500

    2

---
+++ poke/src/pages/home/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import css from './header.module.scss'
+
+export default function header() {
+  return (
+    <div>
+        header estoy aqui
+    </div>
+  )
+}
---
+++ poke/src/pages/home/layout/LayoutHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import css from './layout.module.scss'
+import Header from '../header/Header'
+
+export default function LayoutHome() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
